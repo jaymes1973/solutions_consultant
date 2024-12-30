159d17a7c35603295fdef53948d4564195b4bde5
jaymes1973 <63318344+jaymes1973@users.noreply.github.com>
2024-12-30 13:52:17 +0000

    Update task_app_interactive.py

--- task_app_interactive.py
+++ task_app_interactive.py
@@ -26,7 +26,7 @@ data = load_data("https://raw.githubusercontent.com/jaymes1973/solutions_consult
 
 # Sidebar filters
 st.set_page_config(layout="wide")
-st.sidebar.image("https://github.com/jaymes1973/solutions_consultant/blob/main/image.png?raw=true")
+st.sidebar.image("https://github.com/jaymes1973/solutions_consultant/blob/main/media/image.png?raw=true")
 st.sidebar.header("Filters")
 teams_list=data["team_name"].unique().tolist()
 teams_list.remove("0")
