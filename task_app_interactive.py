b319c5918af4e0ca06a6934cf2330541e97af56a
jaymes1973 <63318344+jaymes1973@users.noreply.github.com>
2025-01-06 16:54:15 +0000

    Update task_app_interactive.py

--- task_app_interactive.py
+++ task_app_interactive.py
@@ -312,6 +312,6 @@ else:
     # Display the interactive plot in Streamlit
     st.plotly_chart(fig)
     st.subheader(f"Game-by-game | {selected_metric.replace('player_match_', '').replace('np_', '').replace('_', ' ').replace('ratio', '%').capitalize().replace('Xg', 'xG').replace('Obv', 'OBV')}")
-    st.text(f"{selected_player_name} {selected_metric.replace('player_match_', '').replace('np_', '').replace('_', ' ').replace('ratio', '%').capitalize()} for all J1 League - 2024 matches in which they have played a minimum of {minimum_mins_played} minutes.")
+    st.text(f"{selected_player_name} {selected_metric.replace('player_match_', '').replace('np_', '').replace('_', ' ').replace('ratio', '%').capitalize()} for all J1 League - 2024 matches in which they have played a minimum of {minimum_mins_played} minutes ({games_considered} games).")
     st.pyplot(fig_bar,transparent=True)
     
